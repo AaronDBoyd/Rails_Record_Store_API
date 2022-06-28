9ef70a406a1c82f4db315c6a17f3aaeb7d4e20bf
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-28 15:27:25 -0700

    add songs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_28_185447) do
+ActiveRecord::Schema.define(version: 2022_06_28_202855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 2022_06_28_185447) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.string "lyrics"
+    t.bigint "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+  end
+
+  add_foreign_key "songs", "albums"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 Album.destroy_all
 
 10.times do |i|
-  Album.create!(name: Faker::Superhero.power, 
+  album = Album.create!(name: Faker::Superhero.power, 
                 genre: Faker::Lorem.word,
                 year: Faker::Number.between(from: 1200, to: 5000))
+
+  5.times do |i|
+    Song.create!(title: Faker::Superhero.name , lyrics: Faker::Lorem.paragraph_by_chars(number:50) , album_id: album.id )
+  end
 end
 p "Created #{Album.count} albums."
+p "Created #{Song.count} songs."
