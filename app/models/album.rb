c59630285034fe342b8583e52314c40eded85698
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-28 15:35:39 -0700

    has_many :songs, dependant: :destroy

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,7 @@
 class Album < ApplicationRecord
+  
+  has_many :songs, dependent: :destroy
+
   validates :name, presence: true
   validates :genre, presence: true
   validates :year, presence: true
