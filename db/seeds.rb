fb852e0292372154718d02b230cb20ec6be432bc
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-28 12:16:52 -0700

    seed albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Album.destroy_all
+
+10.times do |i|
+  Album.create!(name: Faker::Superhero.power, 
+                genre: Faker::Lorem.word,
+                year: Faker::Number.between(from: 1200, to: 5000))
+end
+p "Created #{Album.count} albums."
