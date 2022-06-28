ad0b0271de224f905ae6df0ce5348a68e7cc0b8a
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-28 12:38:42 -0700

    add album actions: index, show, create, update, destroy.

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,38 @@
+class AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+    json_response(@albums)
+  end
+
+  def show
+    @album = Album.find(params[:id])
+    json_response(@album)
+  end
+
+  def create
+    @album = Album.create(album_params)
+    json_response(@album)
+  end
+
+  def update
+    @album = Album.find(params[:id])
+    @album.update(album_params)
+    # added json response
+    json_response(@album)
+  end
+
+  def destroy 
+    @album = Album.find(params[:id])
+    @album.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def album_params
+    params.permit(:name, :genre, :year)
+  end
+end
